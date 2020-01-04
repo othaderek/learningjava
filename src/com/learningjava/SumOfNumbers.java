c0beeed0cdb7a0daf21e3519050c8deb0936a540
othaderek <otha.hernandez@gmail.com>
2020-01-04 18:57:13 -0500

    Added to main method.

--- src/com/learningjava/SumOfNumbers.java
+++ src/com/learningjava/SumOfNumbers.java
@@ -1,12 +1,9 @@
 package com.learningjava;
 
-import java.util.Scanner;
-
 public class SumOfNumbers {
 
     public static void main(String args[]){
-
-        System.out.println(sum(1, 2));
+        System.out.println("Sum: " + sum(1, 2));
     }
 
     public static int sum(int num1, int num2){
