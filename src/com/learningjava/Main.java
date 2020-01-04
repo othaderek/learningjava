2580958f9aef0493ca5c535419483b75af135eb7
othaderek <otha.hernandez@gmail.com>
2020-01-04 17:46:13 -0500

    Initial commit

---
+++ src/com/learningjava/Main.java
@@ -0,0 +1,8 @@
+package com.learningjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/learningjava/PrintStars.java
@@ -0,0 +1,30 @@
+package com.learningjava;
+
+import java.util.Scanner;
+
+public class PrintStars {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        greet();
+        Integer response = scanner.nextInt();
+        countStars(response);
+    }
+
+    public static void greet(){
+        System.out.println("Enter the amount of stars that you want printed.");
+    }
+
+    public static void countStars(Integer integer){
+        String starString = "";
+        for (int i = integer; i > 0; i--){
+            starString += printStar();
+        }
+        System.out.println(starString);
+    }
+
+    public static String printStar(){
+        return "*";
+    }
+
+}
