cdf2a65d14728a394a37404e8eac317e8bb82c96
othaderek <otha.hernandez@gmail.com>
2020-01-04 18:54:26 -0500

    Added Sum of NUmbers class.

---
+++ src/com/learningjava/SumOfNumbers.java
@@ -0,0 +1,16 @@
+package com.learningjava;
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+
+    public static void main(String args[]){
+
+        System.out.println(sum(1, 2));
+    }
+
+    public static int sum(int num1, int num2){
+        return num1 + num2;
+    }
+
+}
