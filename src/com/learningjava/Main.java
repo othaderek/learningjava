3f9282161bb54a23bd14c787c6d59400ce72b340
othaderek <otha.hernandez@gmail.com>
2020-01-05 13:19:36 -0500

    Added prodcut model. Created new instance of Product in main method in Main class.

--- src/com/learningjava/Main.java
+++ src/com/learningjava/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Person person1 = new Person("Otha");
         person1.printPerson();
+
+        Product product1 = new Product("Banana", 1.99, 13);
+        product1.printProduct();
 	// write your code here
     }
 }
