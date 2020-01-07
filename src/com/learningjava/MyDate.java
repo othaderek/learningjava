9b3304b9cdf5231612e957985e00dc91ef4d6491
othaderek <otha.hernandez@gmail.com>
2020-01-07 14:58:27 -0500

    Added setter for each attribute of MyDate class.

--- src/com/learningjava/MyDate.java
+++ src/com/learningjava/MyDate.java
@@ -16,11 +16,24 @@ public class MyDate {
         return day;
     }
 
+    public void setDay(int newDay){
+        this.day = newDay;
+    }
+
     public int getMonth(){
         return month;
+    }
+
+    public void setMonth(int newMonth){
+        this.month = newMonth;
     }
 
     public int getYear(){
         return year;
     }
+
+    public void setYear(int newYear){
+        this.year = newYear;
+    }
+    
 }
